fadca6b96d8e6ade6909a1cc1bb9f201d49f9ed9
Khayelihle Tshuma <khayelihle.tshuma@takealot.com>
2017-02-02 16:52:40 +0200

    Add Rspec support

---
+++ spec/features/creating_activities_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+RSpec.feature "Users can create new projects" do
+  scenario "with valid attributes" do
+    visit "/activities/new"
+
+    fill_in "Name", with: "Sublime Text 3"
+    fill_in "Description", with: "A text editor for everyone"
+    click_button "Add new Activity"
+    expect(page).to have_content "Chillz and Thrillz Become a Partner Login Sign Up Help Name: Sublime Text 3 Description: A text editor for everyone Category: Terms and Privacy Site Map"
+  end
+
+  scenario "Title is correct" do
+    activity = Activity.find_by_id(1)
+    expect(page.current_url).to eq activity_url(activity)
+    title = "Itenarator"
+    expect(page).to have_title title
+  end
+end
