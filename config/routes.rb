270d3f82a187dc1af15735374125a4f74ac833be
Khayelihle Tshuma <khayelihle.tshuma@gmail.com>
2017-02-16 23:46:37 +0200

    Leave strangeness of bitbucket for github

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :providers
+  resources :activities
+	resources :categories
+  resources :home
+  resources :backend_admins
 
-    resources :activities
-		resources :categories
-    resources :home
-    resources :backend_admins
-
-    root 'home#index'
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
 end
